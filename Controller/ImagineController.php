0d11f43b5475a78c6ff9b3e1fd3d70f45523eab6
Adrov Igor <nucleartux@gmail.com>
2012-06-18 19:12:05 +0400

    disable filtering for animated gif

--- Controller/ImagineController.php
+++ Controller/ImagineController.php
@@ -42,8 +42,6 @@ class ImagineController
      */
     private $webRoot;
 
-    private $sourceRoot;
-
     /**
      * Constructs by setting $cachePathResolver
      *
@@ -60,8 +58,7 @@ class ImagineController
         ImagineInterface $imagine,
         FilterManager $filterManager,
         Filesystem $filesystem,
-        $webRoot,
-        $sourceRoot
+        $webRoot
     )
     {
         $this->request           = $request;
@@ -70,7 +67,6 @@ class ImagineController
         $this->filterManager     = $filterManager;
         $this->filesystem        = $filesystem;
         $this->webRoot           = $webRoot;
-        $this->sourceRoot        = $sourceRoot;
     }
 
     /**
@@ -100,19 +96,29 @@ class ImagineController
         }
 
         $realPath = $this->webRoot.$browserPath;
-        $sourcePath = $this->sourceRoot.$path;
+        $sourcePath = $this->webRoot.$path;
+
+        if (!file_exists($sourcePath)) {
+            throw new NotFoundHttpException(sprintf(
+                'Source image not found in "%s"', $sourcePath
+            ));
+        }
+
+        $ext = pathinfo($sourcePath, PATHINFO_EXTENSION);
+        if($ext == 'gif' && $this->isAnimatedGif(fopen($sourcePath, 'r')))
+        {
+            ob_start();
+            echo stream_get_contents(fopen($sourcePath, 'r'));
+            return new Response(ob_get_clean(), 201, array(
+                'content-type' => 'image/gif',
+            ));
+        }
 
         // if the file has already been cached, we're probably not rewriting
         // correctly, hence make a 301 to proper location, so browser remembers
         if (file_exists($realPath)) {
             return new Response('', 301, array(
                 'location' => $this->request->getBasePath().$browserPath
-            ));
-        }
-
-        if (!file_exists($sourcePath)) {
-            throw new NotFoundHttpException(sprintf(
-                'Source image not found in "%s"', $sourcePath
             ));
         }
 
@@ -131,16 +137,39 @@ class ImagineController
             // TODO: get rid of hard-coded quality and format
             $this->filterManager->get($filter)
                 ->apply($this->imagine->open($sourcePath))
-                ->save($realPath, array('quality' => $this->filterManager->getOption($filter, "quality", 100)))
-                ->show($this->filterManager->getOption($filter, "format", "png"));
+                ->save($realPath, array('quality' => 100))
+                ->show('png');
 
             // TODO: add more media headers
             return new Response(ob_get_clean(), 201, array(
-                'content-type' => 'image/' . $this->filterManager->getOption($filter, "format", "png"),
+                'content-type' => 'image/png',
             ));
         } catch (\Exception $e) {
             ob_end_clean();
             throw $e;
         }
+    }
+
+    /**
+     * Detects whether the given GIF image data contains more than one frame
+     *
+     * @param resource $image stream resource containing the binary GIF data
+     * @see http://www.php.net/manual/en/function.imagecreatefromgif.php#88005
+     * @see http://en.wikipedia.org/wiki/Graphics_Interchange_Format#Animated_GIF
+     *
+     * @return boolean true if gif contains more than one frame
+     */
+    protected function isAnimatedGif($image)
+    {
+        //an animated gif contains multiple "frames", with each frame having a
+        //header made up of:
+        // * a static 4-byte sequence (\x00\x21\xF9\x04)
+        // * 4 variable bytes
+        // * a static 2-byte sequence (\x00\x2C) (some variants may use \x00\x21 ?)
+        $count = preg_match_all('#\x00\x21\xF9\x04.{4}\x00(\x2C|\x21)#s', stream_get_contents($image), $m);
+
+        // rewind the stream file pointer to allow further reads from the stream
+        rewind($image);
+        return $count >= 2;
     }
 }
