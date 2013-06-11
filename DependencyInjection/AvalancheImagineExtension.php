5d5413b81897a3a38d153673df1a63736913b053
Geoff Hammond <geoff@geoffhammond.com>
2013-06-11 11:06:38 +0200

    Array replace recursively the configuration
    This allows multiple bundles to configure and override default filters / config values without losing predefined data or appending values to numerical arrays ('size' within filters options)

--- DependencyInjection/AvalancheImagineExtension.php
+++ DependencyInjection/AvalancheImagineExtension.php
@@ -44,7 +44,7 @@ class AvalancheImagineExtension extends Extension
         $config = array();
 
         foreach ($configs as $cnf) {
-            $config = array_merge($config, $cnf);
+            $config = array_replace_recursive($config, $cnf);
         }
 
         return $config;
